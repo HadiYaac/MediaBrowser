9fa25157e29cc3992722d894f9bf9b18a074bc73
HADI <hadi.yaacoub@tedmob.com>
2019-10-17 20:24:00 +0300

    Updated to SDWebImage 5.2.3

--- MediaBrowser/Media.swift
+++ MediaBrowser/Media.swift
@@ -189,9 +189,9 @@ open class Media: NSObject {
         }
     }
 
-    // Load from local file
+    // Load from local file:
     private func performLoadUnderlyingImageAndNotifyWithWebURL(url: URL) {
-        operation = SDWebImageManager.shared().loadImage(with: url, options: [], progress: { (receivedSize, expectedSize, targetURL) in
+        operation = SDWebImageManager.shared.loadImage(with: url, options: [], progress: { (receivedSize, expectedSize, targetURL) in
             let dict = [
             "progress" : min(1.0, CGFloat(receivedSize)/CGFloat(expectedSize)),
             "photo" : self
